9d6d8e96814ccd27446fe54a14dbb2f3cf8e0105
dingyulin <dylwork2003@163.com>
2025-04-10 15:54:36 +0800

    qt embedding采用pre-embedding

---
+++ data_provider/qt_preemb.sh
@@ -0,0 +1,5 @@
+export CUDA_VISIBLE_DEVICES=6
+for target in "Tdew (degC)" "rh (%)" "VPmax (mbar)" "VPact (mbar)" "VPdef (mbar)" "sh (g_kg)" "H2OC (mmol_mol)" "rho (g_m**3)" "wv (m_s)" "max. wv (m_s)" "wd (deg)" "rain (mm)" "raining (s)" "SWDR (W_m2)" "PAR (umol_m2_s)" "max. PAR (umol_m2_s)" "Tlog (degC)" "CO2 (ppm)"
+do
+  python querytext_preembedding.py --target_id "$target"
+done
