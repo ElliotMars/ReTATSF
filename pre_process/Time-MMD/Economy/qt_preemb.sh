8c85292f40ef49e120fc71ce61a7a6435e5c745c
Elliott <dylwork2003@163.com>
2025-07-31 13:59:26 +0800

    删除冗余代码

--- pre_process/Time-MMD/Economy/qt_preemb.sh
+++
@@ -1,5 +0,0 @@
-export CUDA_VISIBLE_DEVICES=0
-for target in "Exports" "Imports" "International Trade Balance"
-do
-  python querytext_preembedding.py --target_id "$target"
-done
--- pre_process/Time-MMD/Energy/querytext_preembedding.py
+++ pre_process/Time-MMD/Energy/querytext_preembedding.py
@@ -9,11 +9,13 @@ import torch
 
 parser = argparse.ArgumentParser(description='Query text Pre-embedding')
 parser.add_argument('--target_id', type=str, required=True, help='target id')
+parser.add_argument('--ForcastingPoint', type=int, required=True, help='ForcastingPoint or Description')
 args = parser.parse_args()
 target_id = args.target_id
+ForcastingPoint = args.ForcastingPoint
 
 BERT_model = 'paraphrase-MiniLM-L6-v2'
-save_dir = f'../../../dataset/Time-MMD/textual/Energy/QueryText-embedding-{BERT_model}/{target_id}'
+save_dir = f'../../../dataset/Time-MMD/textual/Energy/QueryText-embedding-{BERT_model}-Description/{target_id}'
 os.makedirs(save_dir, exist_ok=True)
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -27,38 +29,48 @@ time_span = df['Date'].tolist()
 
 des = pd.read_parquet('../../../dataset/Time-MMD/textual/Energy/QueryTextPackage.parquet')
 
-# 主进度条
-pbar = tqdm.tqdm(total=len(time_span), desc="Encoding")
+if ForcastingPoint:
+    # 主进度条
+    pbar = tqdm.tqdm(total=len(time_span), desc="Encoding")
 
-qts = []
-qts_keys = []
+    qts = []
+    qts_keys = []
 
-for t in time_span:
-    qt = (
-        f"[Forecasting Point]: {t}. "
-        f"[Description]: {des[target_id].loc[0]}"
-    )
-    qts.append(qt)
-    qts_keys.append(t)
+    for t in time_span:
+        qt = (
+            f"[Forecasting Point]: {t}. "
+            #f"[Description]: {des[target_id].loc[0]}"
+        )
+        qts.append(qt)
+        qts_keys.append(t)
 
-    if len(qts) == BATCH_SIZE:
+        if len(qts) == BATCH_SIZE:
+            qts_embedding = model.encode(qts, batch_size=ENCODE_BATCH, show_progress_bar=False)
+            qts_embedding = qts_embedding / np.linalg.norm(qts_embedding, axis=1, keepdims=True)
+
+            for i in range(len(qts)):
+                np.save(os.path.join(save_dir, qts_keys[i][:10] + target_id + '.npy'), qts_embedding[i:i + 1])
+
+            pbar.update(len(qts))
+            qts = []
+            qts_keys = []
+
+    # 最后一个批次
+    if qts:
         qts_embedding = model.encode(qts, batch_size=ENCODE_BATCH, show_progress_bar=False)
         qts_embedding = qts_embedding / np.linalg.norm(qts_embedding, axis=1, keepdims=True)
 
         for i in range(len(qts)):
             np.save(os.path.join(save_dir, qts_keys[i][:10] + target_id + '.npy'), qts_embedding[i:i + 1])
-
         pbar.update(len(qts))
-        qts = []
-        qts_keys = []
 
-# 最后一个批次
-if qts:
-    qts_embedding = model.encode(qts, batch_size=ENCODE_BATCH, show_progress_bar=False)
-    qts_embedding = qts_embedding / np.linalg.norm(qts_embedding, axis=1, keepdims=True)
+    pbar.close()
+else:
+    qt = (
+        f"[Description]: {des[target_id].loc[0]}"
+    )
+    print(qt)
 
-    for i in range(len(qts)):
-        np.save(os.path.join(save_dir, qts_keys[i][:10] + target_id + '.npy'), qts_embedding[i:i + 1])
-    pbar.update(len(qts))
-
-pbar.close()
+    qt_embedding = model.encode(qt, show_progress_bar=False)
+    qt_embedding = qt_embedding / np.linalg.norm(qt_embedding, axis=0, keepdims=True)
+    np.save(os.path.join(save_dir, target_id + '.npy'), qt_embedding)
