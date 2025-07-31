c18b0945afa9bfa98a6f1e5e97d567d06d963e2e
Elliott <dylwork2003@163.com>
2025-07-31 18:17:50 +0800

    单通道

--- pre_process/Time-MMD/Energy/qt_preemb.sh
+++ pre_process/Time-MMD/Energy/qt_preemb.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
-for target in 'Gasoline Prices' 'East Coast' 'New England' 'Central Atlantic' 'Lower Atlantic' 'Midwest' 'Gulf Coast' 'Rocky Mountain' 'West Coast'
+for target in 'Gasoline Prices' #'East Coast' 'New England' 'Central Atlantic' 'Lower Atlantic' 'Midwest' 'Gulf Coast' 'Rocky Mountain' 'West Coast'
 
 do
   python querytext_preembedding.py --target_id "$target" --ForecastingPoint 1
